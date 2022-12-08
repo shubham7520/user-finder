84b1f828e09740b4207ef6b83c7c6fcf662e8410
Shubham Singh <shubhambaba143143@gmail.com>
2022-12-08 21:48:19 +0530

    updated master

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import SearchResult from "./pages/SearchResult";
 import UserResult from "./pages/UserResult";
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route, HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./styles";
 
@@ -10,7 +10,7 @@ function App() {
   const [searchText, setSearchText] = useState("");
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar searchText={searchText} setSearchText={setSearchText} />
       <Routes>
         <Route path="/" exact element={<Home />} />
@@ -20,7 +20,7 @@ function App() {
         />
         <Route path="/user" element={<UserResult />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
